9c6fd2a700e5453d22f22ae3afa898edcc020f03
Wajid Khan <mawk-khan@users.noreply.github.com>
2023-01-11 19:08:00 +0530

    Update wordpress-user-role-body-class.php

--- wordpress-user-role-body-class.php
+++ wordpress-user-role-body-class.php
@@ -1,15 +1,25 @@
-function add_user_role_to_body_classes( $classes ) {
-    global $current_user;
-
-    foreach ( $current_user->roles as $user_role ) {
-        if ( is_admin() ) {
-            $classes .= ' role-' . $user_role;
-        } else {
-            $classes[] = 'role-' . $user_role;
-        }
+/**
+ * Add the user role to the body of the page.
+ */
+add_filter( 'body_class', 'add_user_roles_to_body_class' );
+function add_user_roles_to_body_class( $classes ) {
+    $current_user = wp_get_current_user();
+    $user_roles = (array) $current_user->roles;
+    foreach ( $user_roles as $role ) {
+        $classes[] = 'role-' . $role;
     }
-
     return $classes;
 }
-add_filter( 'body_class', 'add_user_role_to_body_classes', 100 );
-add_filter( 'admin_body_class', 'add_user_role_to_body_classes', 100 );
+
+/**
+ * Add other roles to the list of users.
+ * @param $query_args
+ * @return mixed
+ */
+function add_custom_roles_to_author_field( $query_args ) {
+    if( !current_user_can( 'manage_options' ) )
+        $query_args['role__in'] = [ 'blog_editor' ];
+
+    return $query_args;
+}
+add_filter( 'wp_dropdown_users_args', 'add_custom_roles_to_author_field' );
